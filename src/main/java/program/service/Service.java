fab77ee26b8d2cb43665cf42dba00223604ef75b
Хонько Дмитро <honkodmitro@Honkos-MacBook-Air.local>
2020-12-19 03:06:46 +0200

    form

---
+++ src/main/java/program/service/Service.java
@@ -0,0 +1,23 @@
+package program.service;
+
+import java.io.File;
+
+public class Service {
+    private static Service instance;
+
+    private Service() {
+    }
+
+    public static Service getInstance() {
+        if (instance == null) {
+            instance = new Service();
+        }
+        return instance;
+    }
+
+    public void openFile(File file) {
+        
+    }
+
+
+}
