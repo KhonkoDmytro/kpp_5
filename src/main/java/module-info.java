3692d874b062f52ad1fc98a284eb55f1c880d594
Illia Prytula <pritularllya@gmail.com>
2020-12-19 19:11:14 +0200

    Made changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.xml;
 
     opens program.controller to javafx.fxml;
     opens program to javafx.fxml;
