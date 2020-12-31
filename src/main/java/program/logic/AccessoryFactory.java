9ec2e2638cec8741d5e988a5810164d4d0e6c2f9
dansad <danylotv43@gmail.com>
2020-12-31 13:14:57 +0200

    Cleanup

--- src/main/java/program/logic/AccessoryFactory.java
+++ src/main/java/program/logic/AccessoryFactory.java
@@ -1,7 +1,5 @@
 package program.logic;
 
-import program.logic.ParticleFactory;
-
 public class AccessoryFactory implements ParticleFactory {
     public CarParticle get() {
         return new Accessory();
--- src/main/java/program/logic/ParticleFactory.java
+++ src/main/java/program/logic/ParticleFactory.java
@@ -1,6 +1,5 @@
 package program.logic;
 
-// На дяконюк маєш фабрику
 public interface ParticleFactory {
     CarParticle get();
 }
